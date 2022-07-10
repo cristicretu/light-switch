2fea3cfd8c80262b209da2eea088ad3f9991cc34
Cristian Crețu <crisemcr@gmail.com>
2022-07-10 18:12:49 +0300

    feat: break the bulb

--- components/Lightbulb/Lightbulb.tsx
+++ components/Lightbulb/Lightbulb.tsx
@@ -6,8 +6,33 @@ import useSound from 'use-sound'
 export default function Lightbulb() {
   const [on] = useSound('/static/audio/on.mp3')
   const [off] = useSound('/static/audio/off.mp3')
+  const [destroy] = useSound('/static/audio/break.mp3')
   const [, setMounted] = useState<boolean>(false)
   const { resolvedTheme, setTheme } = useTheme()
+  const [clicks, setClicks] = useState<number>(0)
+  const [broken, setBroken] = useState<boolean>(false)
+  const [clickedFirstTime, setClickedFirstTime] = useState<boolean>(true)
+
+  const handleClick = () => {
+    if (clickedFirstTime) {
+      setClicks(1)
+      setClickedFirstTime(false)
+    } else {
+      setClicks(clicks + 1)
+    }
+  }
+
+  // setfirstclicked to false after 10 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      setClickedFirstTime(true)
+    }, 10000)
+    if (clicks > 10) {
+      destroy()
+      setBroken(true)
+      setTheme('dark')
+    }
+  }, [clickedFirstTime, clicks, destroy, setTheme])
 
   useEffect(() => {
     setMounted(true)
@@ -21,21 +46,24 @@ export default function Lightbulb() {
         <div className='strip'></div>
       </div>
 
-      <div
-        className='bulb cursor-pointer'
-        onClick={() => {
-          if (resolvedTheme === 'dark') {
-            on()
-          } else {
-            off()
-          }
-          setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-        }}
-      >
-        <div className='zig'></div>
-        <div className='zig'></div>
-        <div className='zig'></div>
-      </div>
+      {!broken && (
+        <div
+          className='bulb cursor-pointer'
+          onClick={() => {
+            if (resolvedTheme === 'dark') {
+              on()
+            } else {
+              off()
+            }
+            setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+            handleClick()
+          }}
+        >
+          <div className='zig'></div>
+          <div className='zig'></div>
+          <div className='zig'></div>
+        </div>
+      )}
     </div>
   )
 }
