688c8e517a58409007f1c35547e48c063b3ef769
Cristian Crețu <crisemcr@gmail.com>
2022-07-10 19:49:06 +0300

    fix: responsive

--- components/Lightbulb/Lightbulb.tsx
+++ components/Lightbulb/Lightbulb.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useTheme } from 'next-themes'
 import useSound from 'use-sound'
@@ -7,33 +7,48 @@ export default function Lightbulb() {
   const [on] = useSound('/static/audio/on.mp3')
   const [off] = useSound('/static/audio/off.mp3')
   const [destroy] = useSound('/static/audio/break.mp3')
-  const [, setMounted] = useState<boolean>(false)
+
+  const [mounted, setMounted] = useState<boolean>(false)
   const { resolvedTheme, setTheme } = useTheme()
+
   const [clicks, setClicks] = useState<number>(0)
   const [broken, setBroken] = useState<boolean>(false)
   const [clickedFirstTime, setClickedFirstTime] = useState<boolean>(true)
 
   const handleClick = () => {
     if (clickedFirstTime) {
-      setClicks(1)
+      setClicks(0)
       setClickedFirstTime(false)
     } else {
       setClicks(clicks + 1)
     }
   }
 
-  // setfirstclicked to false after 10 seconds
+  const handleLightSwitch = () => {
+    if (resolvedTheme === 'dark') {
+      on()
+      setTheme('light')
+    } else {
+      off()
+      setTheme('dark')
+    }
+    handleClick()
+  }
+
   useEffect(() => {
     setTimeout(() => {
       setClickedFirstTime(true)
-    }, 10000)
-    if (clicks > 10) {
-      destroy()
-      setBroken(true)
-      setTheme('dark')
-    }
-  }, [clickedFirstTime, clicks, destroy, setTheme])
+    }, 5000)
+  }, [])
 
+  useEffect(() => {
+    if (clicks > 4) {
+      setBroken(true)
+      destroy()
+    }
+  }, [clicks, destroy])
+
+  // We can change the theme after the component has mounted
   useEffect(() => {
     setMounted(true)
   }, [])
@@ -46,19 +61,8 @@ export default function Lightbulb() {
         <div className='strip'></div>
       </div>
 
-      {!broken && (
-        <div
-          className='bulb cursor-pointer'
-          onClick={() => {
-            if (resolvedTheme === 'dark') {
-              on()
-            } else {
-              off()
-            }
-            setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-            handleClick()
-          }}
-        >
+      {!broken && mounted && (
+        <div className='bulb cursor-pointer' onClick={handleLightSwitch}>
           <div className='zig'></div>
           <div className='zig'></div>
           <div className='zig'></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Container from 'components/Container'
 export default function Home() {
   return (
     <Container>
-      <div className='inset-0 max-w-2xl mx-auto mt-64 md:mt-48 flex flex-col gap-8'>
+      <div className='inset-0 max-w-2xl mx-auto mt-64 md:mt-48 flex flex-col gap-8 p-4'>
         <h1 className='text-4xl font-bold bg-gradient-to-b lg:bg-gradient-to-r from-yellow-300 to-yellow-900/50 dark:from-gray-500/60 dark:to-gray-500/60 bg-clip-text text-transparent'>
           Human Interface Guidelines: The Apple Desktop Interface
         </h1>
